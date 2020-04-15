ad8c535b763b0311d1a87d68bcbafd9331493d1f
Josh Birnholz <josh@birnholz.com>
2020-04-15 14:44:53 -0400

    Vapor works test

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,7 +4,7 @@ import Vapor
 public func routes(_ router: Router) throws {
     // Basic "It works" example
     router.get { req in
-        return "It works!"
+        return "Vapor works!"
     }
     
     // Basic "Hello, world!" example

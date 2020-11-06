151d482045121b60530e9496a4867e906a2fdb1e
Josh Birnholz <josh@birnholz.com>
2020-11-06 14:23:38 -0500

    Fix random

--- Sources/App/Generation/String+Regex.swift
+++ Sources/App/Generation/String+Regex.swift
@@ -67,7 +67,7 @@ extension Array {
 	public func choose(_ n: Int) -> [Element] {
 		var copy = self
 		for i in stride(from: count - 1, to: count - n - 1, by: -1) {
-			copy.swapAt(i, Int(arc4random_uniform(UInt32(i + 1))))
+			copy.swapAt(i, Int.random(in: 0..<(i+1)))
 		}
 		return Array(copy.suffix(n))
 	}

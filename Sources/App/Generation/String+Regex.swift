e23a902a8e6961e813d216e9be19b78e40cd3f44
Josh Birnholz <josh@birnholz.com>
2020-11-07 11:49:33 -0500

    Fix random choosing

--- Sources/App/Generation/String+Regex.swift
+++ Sources/App/Generation/String+Regex.swift
@@ -65,6 +65,9 @@ extension Array {
 	}
 	
 	public func choose(_ n: Int) -> [Element] {
+		guard n >= 0 else { return [] }
+		let n = Swift.min(n, count)
+		
 		var copy = self
 		for i in stride(from: count - 1, to: count - n - 1, by: -1) {
 			copy.swapAt(i, Int.random(in: 0..<(i+1)))
